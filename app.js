7c39323fbcf6f9b3d28f2248adfb6bf47ad698db
Jaime González <jaime@funed.tech>
2023-07-16 12:50:28 -0600

    Sets second level of authentication

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  `mongodb+srv://jaime:1nHdXlr8DQ6owE0U@security-cluster.frmzjfb.mongodb.net/?retryWrites=true&w=majority`
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully to MongoDB");
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const mySecret = "Esteeselpinchesecreto";
+userSchema.plugin(encrypt, { secret: mySecret, encryptedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser
+    .save()
+    .then(function () {
+      res.render("secrets");
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username })
+    .then((user) => {
+    //   console.log(user);
+      if (user && user.password === password) {
+        res.render("secrets");
+      } else {
+        res.send("You are not welcome");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen("3000", (req, res) => {
+  console.log("Server running on port 3000");
+});
